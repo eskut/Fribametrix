260e8ff4296143834a8eceb8ab8a8d4a51c626a7
eskut <esko.taivalmaki@edu.sasky.fi>
2022-07-05 15:23:24 +0300

    Items and item components added

---
+++ src/components/item/item.js
@@ -0,0 +1,17 @@
+import styles from './item.module.scss';
+
+function Item(props) {
+    return (
+        <div className={styles.item}>
+            <div className={styles.item_data}>
+                <div className={styles.item_place}>Vampula</div>
+                <div className={styles.item_player}>Jaska</div>
+                <div className={styles.item_count}>18 väylää</div>
+                <div className={styles.item_result}>-5</div>
+                <div className={styles.item_date}>13.06.2022</div>
+            </div>
+        </div>
+    );
+}
+
+export default Item;
