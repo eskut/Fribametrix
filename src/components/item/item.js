b2e34404971c1ed1ed8b8917d73b53aa25d57de4
eskut <esko.taivalmaki@edu.sasky.fi>
2022-07-14 11:35:48 +0300

    Testdata added

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -2,14 +2,18 @@ import styles from './item.module.scss';
 import { MdNavigateNext } from 'react-icons/md';
 
 function Item(props) {
+
+    const locale = "fi-FI";
+    const playDate = new Date(props.data.playdate).toLocaleDateString(locale);
+
     return (
         <div className={styles.item}>
             <div className={styles.item_data}>
-                <div className={styles.item_place}>Vampula</div>
-                <div className={styles.item_player}>Jaska</div>
-                <div className={styles.item_count}>18 väylää</div>
-                <div className={styles.item_result}>-5</div>
-                <div className={styles.item_date}>13.06.2022</div>
+                <div className={styles.item_place}>{props.data.place}</div>
+                <div className={styles.item_player}>{props.data.player}</div>
+                <div className={styles.item_count}>{props.data.count} väylää</div>
+                <div className={styles.item_result}>{props.data.result}</div>
+                <div className={styles.item_playdate}>{playDate}</div>
             </div>
             <div className={styles.item_edit}>
                 <MdNavigateNext />
---
+++ src/testdata.js
@@ -0,0 +1,44 @@
+const data = [
+    {
+        id:      "1",
+        place:   "Vampula",
+        count:   18,
+        playdate:    "2022-04-13",
+        player:  "Jaska",
+        result:  -5
+    },
+    {
+        id:      "2",
+        place:   "Huittinen",
+        count:   18,
+        playdate:    "2022-04-20",
+        player:  "Jaska",
+        result:  0
+    },
+    {
+        id:      "3",
+        place:   "Pitkäjärvi",
+        count:   18,
+        playdate:    "2022-05-12",
+        player:  "Jaska",
+        result:  0
+    },
+    {
+        id:      "4",
+        place:   "Rekikoski",
+        count:   9,
+        playdate:    "2022-06-13",
+        player:  "Jaska",
+        result:  0
+    },
+    {
+        id:      "5",
+        place:   "Äetsä",
+        count:   18,
+        playdate:    "2022-07-01",
+        player:  "Jaska",
+        result:  1
+    },
+];
+
+export default data;
